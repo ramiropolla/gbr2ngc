ce87fe4f9d81ac5937453fb0ce094935a581a6bd
Ramiro Polla <ramiro.polla@gmail.com>
2023-02-23 05:16:22 +0100

    remove 3opt and some printf

--- src/gbr2ngc_export.cpp
+++ src/gbr2ngc_export.cpp
@@ -142,7 +142,6 @@ static void reorder_paths(std::vector<city_t> *_dst, const Paths &src)
     prev_i = i;
   }
   total_cost += costs[prev_i * costs_size + n_total];
-  printf("initial_cost %" PRId64 "\n", total_cost);
 
   // nearest neighbor
   std::vector<city_t> result_nn(n_total);
@@ -172,17 +171,11 @@ static void reorder_paths(std::vector<city_t> *_dst, const Paths &src)
     prev_i = best_j;
   }
   total_cost += costs[prev_i * costs_size + n_total];
-  printf("nn_cost %" PRId64 "\n", total_cost);
   free(used);
 
   // 2-opt
-//   std::vector<city_t> result_2opt(n_total);
   std::vector<city_t> result_2opt = result_nn;
-#if 1
 _2opt:
-#if 0
-  for ( city_t x : result_2opt ) { printf("%3zu,", (size_t) x); } printf("\n");
-#endif
   for ( city_t i = -1; i < (n_total - 2); i++ )
   {
     city_t i0_idx = (i == -1) ? n_total : result_2opt[i];
@@ -191,15 +184,10 @@ _2opt:
     {
       city_t j0_idx = result_2opt[j];
       city_t j1_idx = (j == (n_total - 1)) ? n_total : result_2opt[j + 1];
-      int64_t delta = costs[i0_idx * costs_size + j0_idx] + costs[i1_idx * costs_size + j1_idx]
-                    - costs[i0_idx * costs_size + i1_idx] - costs[j0_idx * costs_size + j1_idx];
-#if 0
-      printf("+cost[%zu][%zu]: %" PRId64 "\n", (size_t) i0_idx, (size_t) j0_idx, costs[i0_idx * costs_size + j0_idx]);
-      printf("+cost[%zu][%zu]: %" PRId64 "\n", (size_t) i1_idx, (size_t) j1_idx, costs[i1_idx * costs_size + j1_idx]);
-      printf("-cost[%zu][%zu]: %" PRId64 "\n", (size_t) i0_idx, (size_t) i1_idx, costs[i0_idx * costs_size + i1_idx]);
-      printf("-cost[%zu][%zu]: %" PRId64 "\n", (size_t) j0_idx, (size_t) j1_idx, costs[j0_idx * costs_size + j1_idx]);
-      printf("delta[%zu][%zu]: %" PRId64 "\n", (size_t) i, (size_t) j, delta);
-#endif
+      int64_t delta = costs[i0_idx * costs_size + j0_idx]
+                    + costs[i1_idx * costs_size + j1_idx]
+                    - costs[i0_idx * costs_size + i1_idx]
+                    - costs[j0_idx * costs_size + j1_idx];
       if ( delta < 0 )
       {
         std::reverse(result_2opt.begin() + i + 1, result_2opt.begin() + j + 1);
@@ -208,104 +196,12 @@ _2opt:
       }
     }
   }
-  printf("2opt_cost %" PRId64 "\n", total_cost);
-  for ( city_t x : result_2opt ) { printf("%3zu,", (size_t) x); } printf("\n");
-#endif
-
-  // 3-opt
-//   std::vector<city_t> result_3opt(n_total);
-  std::vector<city_t> result_3opt = result_2opt;
-#if 1
-_3opt:
-#if 0
-  for ( city_t x : result_3opt ) { printf("%3zu,", (size_t) x); } printf("\n");
-#endif
-  // 0...i...j...k...m...0
-  for ( city_t i = -1; i < (n_total - 3); i++ )
-  {
-    city_t i0_idx = (i == -1) ? n_total : result_3opt[i];
-    city_t i1_idx = result_3opt[i + 1];
-    for ( city_t j = i + 1; j < (n_total - 2); j++ )
-    {
-      city_t j0_idx = result_3opt[j];
-      city_t j1_idx = result_3opt[j + 1];
-      for ( city_t k = j + 1; k < (n_total - 1); k++ )
-      {
-        city_t k0_idx = result_3opt[k];
-        city_t k1_idx = result_3opt[k + 1];
-        for ( city_t m = k + 1; m < n_total; m++ )
-        {
-          city_t m0_idx = result_3opt[m];
-          city_t m1_idx = (m == (n_total - 1)) ? n_total : result_3opt[m + 1];
-          for ( int n = 1; n < 8; n++ )
-          {
-            int64_t delta = 0;
-#if 1
-            if ( n & 0x01 )
-              delta += costs[k0_idx * costs_size + m0_idx] + costs[k1_idx * costs_size + m1_idx] - costs[k0_idx * costs_size + k1_idx] - costs[m0_idx * costs_size + m1_idx];
-            if ( n & 0x02 )
-              delta += costs[j0_idx * costs_size + k0_idx] + costs[j1_idx * costs_size + k1_idx] - costs[j0_idx * costs_size + j1_idx] - costs[k0_idx * costs_size + k1_idx];
-            if ( n & 0x04 )
-              delta += costs[i0_idx * costs_size + j0_idx] + costs[i1_idx * costs_size + j1_idx] - costs[i0_idx * costs_size + i1_idx] - costs[j0_idx * costs_size + j1_idx];
-#else
-            switch ( n )
-            {
-            case 1: // 001
-              delta = costs[k0_idx * costs_size + m0_idx] + costs[k1_idx * costs_size + m1_idx]
-                    - costs[k0_idx * costs_size + k1_idx] - costs[m0_idx * costs_size + m1_idx];
-              break;
-            case 2: // 010
-              delta = costs[j0_idx * costs_size + k0_idx] + costs[j1_idx * costs_size + k1_idx]
-                    - costs[j0_idx * costs_size + j1_idx] - costs[k0_idx * costs_size + k1_idx];
-              break;
-            case 3: // 011
-              delta = costs[j0_idx * costs_size + m0_idx] + costs[j1_idx * costs_size + m1_idx]
-                    - costs[j0_idx * costs_size + j1_idx] - costs[m0_idx * costs_size + m1_idx];
-              break;
-            case 4: // 100
-              delta = costs[i0_idx * costs_size + j0_idx] + costs[i1_idx * costs_size + j1_idx]
-                    - costs[i0_idx * costs_size + i1_idx] - costs[j0_idx * costs_size + j1_idx];
-              break;
-            case 5: // 101
-              delta = costs[i0_idx * costs_size + j0_idx] + costs[i1_idx * costs_size + j1_idx]
-                    + costs[k0_idx * costs_size + m0_idx] + costs[k1_idx * costs_size + m1_idx]
-                    - costs[i0_idx * costs_size + i1_idx] - costs[j0_idx * costs_size + j1_idx]
-                    - costs[k0_idx * costs_size + k1_idx] - costs[m0_idx * costs_size + m1_idx];
-              break;
-            case 6: // 110
-              delta = costs[i0_idx * costs_size + k0_idx] + costs[i1_idx * costs_size + k1_idx]
-                    - costs[i0_idx * costs_size + i1_idx] - costs[k0_idx * costs_size + k1_idx];
-              break;
-            case 7: // 111
-              delta = costs[i0_idx * costs_size + m0_idx] + costs[i1_idx * costs_size + m1_idx]
-                    - costs[i0_idx * costs_size + i1_idx] - costs[m0_idx * costs_size + m1_idx];
-              break;
-            }
-#endif
-            if ( delta >= 0 )
-              continue;
-            if ( n & 0x01 )
-              std::reverse(result_3opt.begin() + k + 1, result_3opt.begin() + m + 1);
-            if ( n & 0x02 )
-              std::reverse(result_3opt.begin() + j + 1, result_3opt.begin() + k + 1);
-            if ( n & 0x04 )
-              std::reverse(result_3opt.begin() + i + 1, result_3opt.begin() + j + 1);
-            total_cost += delta;
-            goto _3opt;
-          }
-        }
-      }
-    }
-  }
-  printf("3opt_cost %" PRId64 "\n", total_cost);
-  for ( city_t x : result_3opt ) { printf("%3zu,", (size_t) x); } printf("\n");
-#endif
 
   free(costs);
 
   dst.resize(n_total);
   for ( city_t i = 0; i < n_total; i++ )
-    dst[i] = result_map[result_3opt[i]];
+    dst[i] = result_map[result_2opt[i]];
 }
 
 int export_paths_to_gcode_unit( FILE *ofp, const Paths &paths, int src_units_0in_1mm, int dst_units_0in_1mm, double ds)
